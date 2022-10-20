e103e801eae3d0bd70e6723f6a39f7df95b5c5a5
WandreyLima <nejihyuuga0@gmail.com>
2022-10-20 16:22:19 -0300

    pagina galeria

---
+++ js/galeria.js
@@ -0,0 +1,24 @@
+let imagens = document.querySelectorAll(".galeria-card");
+let modal = document.querySelector(".modal");
+let modalImg = document.querySelector("#modal-img");
+let btnClose = document.querySelector("#btn-close");
+let srcVal; // Armazenar o src da nossa imagem
+
+for (let i = 0; i < imagens.length; i++) {
+  // Para cada imagem na posição i, adicionar o evento click com a function
+  imagens[i].addEventListener("click", function () {
+    srcVal = imagens[i].getAttribute("src"); // Pega o src de cada imagem em i
+    modalImg.setAttribute("src", srcVal); // recebe o valor do src pegado acima
+    modal.classList.toggle("modal_active"); //muda pra classe moda_active que tem o objetivo de mudar o visible do elemento
+  });
+}
+
+btnClose.addEventListener("click", function () {
+  modal.classList.toggle("modal_active");
+});
+
+document.body.addEventListener("keyup", function (e) {
+  if (e.key === 'Escape') {
+    modal.classList.toggle("modal_active");
+  }
+});
